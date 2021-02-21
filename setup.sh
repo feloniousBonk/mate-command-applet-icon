e0108a558e701ee03b6479e09460849837869000
feloniousbonk <feloniousbonk@protonmail.com>
2021-02-21 12:45:04 -0700

    setup.sh - latest file config script 2/21/2021

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e "cloning mate-applets repository and replacing appropriate command-applet files, please wait...\n===============================";
-rm -rf ./docs/command-applet-icon.png;
+rm ./docs/command-applet-icon.png;
 git clone https://github.com/mate-desktop/mate-applets.git;
 echo -e "\nreplacing command-applet source files....";
 cp command-preferences.ui mate-applets/command/data;    
