2c652af48cc460d114d0e25a1395ad71eeb4cf77
feloniousbonk <feloniousbonk@protonmail.com>
2021-02-21 12:12:08 -0700

    install.sh - latest file install script 2/21/2021

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -rf ./docs/command-applet-icon.png
-git clone https://github.com/mate-desktop/mate-applets.git;    
+echo -e "cloning mate-applets repository and replacing appropriate command-applet files, please wait...\n===============================";
+rm -rf ./docs/command-applet-icon.png;
+git clone https://github.com/mate-desktop/mate-applets.git;
+echo -e "\nreplacing command-applet source files....";
 cp command-preferences.ui mate-applets/command/data;    
 cp org.mate.panel.applet.command.gschema.xml.in mate-applets/command/data;    
-cp command.c mate-applets/command/src   
+cp command.c mate-applets/command/src;   
+echo -e "===============================\ninstall.sh has completed..."
