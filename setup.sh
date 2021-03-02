be970fce268f417d26439a5027961c74d94c681a
feloniousbonk <feloniousbonk@protonmail.com>
2021-03-01 19:27:19 -0700

    setup.sh - latest file config script 3/1/2021

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+DATA_DIR="mate-applets/command/data"
+SRC_DIR="mate-applets/command/src"
+
 echo -e "cloning mate-applets repository and replacing appropriate command-applet files, please wait...\n===============================";
 rm ./docs/command-applet-icon.png;
 git clone https://github.com/mate-desktop/mate-applets.git;
 echo -e "\nreplacing command-applet source files....";
-cp command-preferences.ui mate-applets/command/data;    
-cp org.mate.panel.applet.command.gschema.xml.in mate-applets/command/data;    
-cp command.c mate-applets/command/src;   
+cp command-preferences.ui $DATA_DIR;    
+cp org.mate.panel.applet.command.gschema.xml.in $DATA_DIR;    
+cp command.c $SRC_DIR;   
 echo -e "===============================\nsetup.sh has completed..."
